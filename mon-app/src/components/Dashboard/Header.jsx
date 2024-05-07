422ba855613120134722f2b6fbda4b6e623f936d
sahineFaniria <faniriasahine@gmail.com>
2024-05-07 22:20:44 +0300

    Modal

--- mon-app/src/components/Dashboard/Header.jsx
+++ mon-app/src/components/Dashboard/Header.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import setting from "../../images/settings.svg";
-import { Input } from '../ui/input';
-import { Label } from "../ui/label";
-import { Button } from '../ui/button';
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui/sheet";
 import Notif from './Modal/Notif';
+import Right from './Modal/Right';
 
 
 const Header = () => {
@@ -26,41 +13,9 @@ const Header = () => {
                 <div className='flex items-center'>
                     <Notif/>
                 </div>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button className='rounded-lg' size='sm' variant="outline"><img className=' cursor-pointer' src={setting} alt="" /></Button>
-                    </SheetTrigger>
-                    <SheetContent>
-                        <SheetHeader>
-                        <SheetTitle>Modifier Profile</SheetTitle>
-                        <SheetDescription>
-                            Changer votre profile ici. Cliquer sur enregistrer vous avez fini.
-                        </SheetDescription>
-                        </SheetHeader>
-                        <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-start">
-                            Farm:
-                            </Label>
-                            <Input id="name" value="Farm name" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="username" className="text-start">
-                            Utilisateur:
-                            </Label>
-                            <Input id="username" value="@user x" className="col-span-3" />
-                        </div>
-                        </div>
-                        <SheetFooter>
-                        <SheetClose asChild>
-                            <Button className='text-white' size='lg' variant='rafane' type="submit">Enregistrer</Button>
-                        </SheetClose>
-                        </SheetFooter>
-                    </SheetContent>
-                </Sheet>
-                
-                
-                
+
+                <Right/>
+               
             </div>
             
         </div>
